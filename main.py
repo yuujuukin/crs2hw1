7a8d928e226649dbb203c588d7ace90da4d11c47
yuujuukin <106178276+yuujuukin@users.noreply.github.com>
2022-12-19 15:01:47 +0300

    //

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-from flask import Flaskfrom utils import *app = Flask(__name__)@app.route("/")def page_index():    candidates: list[dict] = get_all_candidates()    result = format_candidates(candidates)    return result@app.route("/candidates/<int:pk>")def get_candidate(pk):    candidate: dict = get_all_candidates(pk)    result: str = format_candidates([candidate])    return resultapp.run(port=5050)
+from flask import Flask
+
+from utils import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_index():
+    candidates: list[dict] = get_all_candidates()
+    result = format_candidates(candidates)
+    return result
+
+
+@app.route("/candidates/<int:pk>")
+def get_candidate(pk):
+    candidate: dict = get_all_candidates(pk)
+    result: str = format_candidates([candidate])
+    return result
+
+
+app.run(port=5050)
+#
