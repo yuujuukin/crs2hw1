d7c19fdd3d4eafbb2bb99edee7153d6f07fd2317
yuujuukin <106178276+yuujuukin@users.noreply.github.com>
2022-12-21 09:30:53 +0300

    that work

--- utils.py
+++ utils.py
@@ -3,39 +3,39 @@ import json
 
 def load_json() -> list[dict]:
     with open('candidates.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        return data
-
-
-def format_candidates(candidates: list[dict]) -> str:
-    result = '<pre>'
-
-    for candidate in load_json():
-        result += f""""
-                    {candidate["name"]}\n
-                    {candidate["position"]}\n
-                    {candidate["skills"]}\n
-                    """
-    result += '</pre>'
-    return result
+        candidates = json.load(f)
+        return candidates
 
 
 def get_all_candidates() -> list[dict]:
     return load_json()
 
 
+def format_candidates(candidates):
+    result = '<pre>'
+
+    for item in candidates:
+        result += f""""
+                    {item["name"]}\n
+                    {item["position"]}\n
+                    {item["skills"]}\n
+                    """
+    result += '</pre>'
+    return result
+
+
 def get_candidate_by_pk(pk: int):
     candidates = get_all_candidates()
-    for candidate in candidates:
-        if candidate['pk'] == pk:
-            return candidate
+    for item in candidates:
+        if item['pk'] == pk:
+            return item
     return None
 
 
-# def get_by_skill(skill_name, data):
-#     arr = []
-#     for item in data:
-#         if skill_name in item['skills']:
-#             arr.append(item)
-#             print(arr)
-#             return arr
+def get_by_skill(skill: str) -> list[dict]:
+    candidates = get_all_candidates()
+    arr = []
+    for item in candidates:
+        if skill in item['skills'].lower().split(', '):
+            arr.append(item)
+    return arr
